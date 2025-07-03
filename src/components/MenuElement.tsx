1e262ec1d8bf66c2b521d5ebfed08e1d8226097f
Cris1277 <aztecafran@gmail.com>
2025-07-03 02:49:13 +0200

    Update MenuElement.tsx

--- src/components/MenuElement.tsx
+++ src/components/MenuElement.tsx
@@ -1,18 +1,24 @@
-import { MenuItem } from "../types"
+import { MenuItem } from "../types";
 
+// Definimos el tipo de las props que recibe el componente
 type MenuElementProps = {
-    item :MenuItem
-    addItem: (item:MenuItem) => void
-}
-export default function MenuElement({item, addItem}:MenuElementProps) {
-  
-    return (
-  
-        <button className="font black flex justify-between w-full bg-teal-200 p-5 hover:bg-teal-400 rounded-xl " onClick={()=>addItem(item)}>
-            <p className="text-1xl font-bold">{item.name} </p>
-            <p className="font-black">{item.price}€</p>
-        </button>
-        
+  item: MenuItem;                      // Objeto con los datos del producto
+  addItem: (item: MenuItem) => void;  // Función que añade el ítem al pedido
+};
 
-  )
+// Componente funcional que representa un ítem del menú
+export default function MenuElement({ item, addItem }: MenuElementProps) {
+  return (
+    // Botón que al hacer clic agrega el ítem a la orden
+    <button
+      className="font-black flex justify-between w-full bg-teal-200 p-5 hover:bg-teal-400 rounded-xl"
+      onClick={() => addItem(item)}
+    >
+      {/* Nombre del producto */}
+      <p className="text-1xl font-bold">{item.name}</p>
+
+      {/* Precio del producto */}
+      <p className="font-black">{item.price}€</p>
+    </button>
+  );
 }
